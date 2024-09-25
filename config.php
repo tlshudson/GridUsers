45465419c5fe622179fcdea3b60f22dc47980dee
tlshudson <hudsonteles00@gmail.com>
2024-09-25 18:06:15 -0300

    Add files GridUsers

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'tlshudson');
+    define('PASS', '985559961');
+    define('BASE', 'cadastro');
+
+    $conn = new MySQLi(HOST,USER,PASS,BASE);
+?>
